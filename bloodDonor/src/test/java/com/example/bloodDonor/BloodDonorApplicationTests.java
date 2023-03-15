4cd82aa29a7a2dd7247dad351006196e85cc3fa5
cmananthu <cmananthakrishnan1@gmail.com>
2023-03-15 21:56:48 +0530

    Added Junit for Main class.

--- bloodDonor/src/test/java/com/example/bloodDonor/BloodDonorApplicationTests.java
+++ bloodDonor/src/test/java/com/example/bloodDonor/BloodDonorApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.bloodDonor;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.project.bloodDonor.BloodDonorApplication;
+
+@ExtendWith(MockitoExtension.class)
+
 class BloodDonorApplicationTests {
 
 	@Test
 	void contextLoads() {
+		BloodDonorApplication.main(new String[] {});
 	}
 
 }
