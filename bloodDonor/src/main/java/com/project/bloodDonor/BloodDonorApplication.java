3231ba79283326c5cf6d4c74b6ad9bcc5148899d
cmananthu <cmananthakrishnan1@gmail.com>
2023-03-17 09:26:46 +0530

    Added Swagger-open API features.

--- bloodDonor/src/main/java/com/project/bloodDonor/BloodDonorApplication.java
+++ bloodDonor/src/main/java/com/project/bloodDonor/BloodDonorApplication.java
@@ -3,8 +3,13 @@ package com.project.bloodDonor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Blood Donor API Documentation",description="Blood Donor API manages the details of all the donors registered in the database. It basically do the CRUD operations.", version = "1.6.4"))
+
 public class BloodDonorApplication {
 
 	public static void main(String[] args) {
--- bloodDonor/src/main/java/com/project/bloodDonor/entity/BloodDonorDAO.java
+++ bloodDonor/src/main/java/com/project/bloodDonor/entity/BloodDonorDAO.java
@@ -7,6 +7,7 @@ import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,14 +22,19 @@ public class BloodDonorDAO {
 	private int id;
 	@JsonProperty("name")
 	@NotBlank(message = "Name is mandatory")
+	 @Schema(description = "The donor's name")
 	private String donorName;
+	 @Schema(description = "Valid phone number")
 	private String phone;
 	@Email(message="email should be valid")
 	@NotBlank(message = "Email is mandatory")
+	 @Schema(description = "Email should be valid and is mandatory")
 	private String email;
 	@NotNull(message="add a blood group")
+	 @Schema(description = "Blood group should not be null")
 	private String bloodGroup;
 	private String gender;
+	 @Schema(description = "age should be greater than 18")
 	@Min(value=18,message="age should be greater than 18")
 	private String age;
 	private double height;
